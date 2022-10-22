579ae624afddf0590ea378a18a6c5fb2c1113b2a
zimbaldi <italo.zimbaldi@yahoo.com.br>
2022-10-22 09:01:01 -0300

    First commit, finished UI

---
+++ lib/components/button_row.dart
@@ -0,0 +1,32 @@
+import 'package:calculator/components/button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'button.dart';
+
+class ButtonRow extends StatelessWidget {
+  final List<Button> buttons;
+
+  ButtonRow(this.buttons);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: buttons.fold(<Widget>[], (list, b) {
+          list.isEmpty
+              ? list.add(b)
+              : list.addAll([
+                  SizedBox(
+                    width: 1,
+                  ),
+                  b
+                ]);
+          return list;
+        }),
+      ),
+    );
+  }
+}
